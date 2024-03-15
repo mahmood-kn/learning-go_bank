7e2c5a4093975b7e37f2568daca99d84962ad81d
Mahmood <mahmood.kn1526@gmail.com>
2024-03-15 21:37:58 +0330

    2-33

--- bank.go
+++ bank.go
@@ -22,12 +22,26 @@ func main() {
 		fmt.Print("Your Deposit: ")
 		var depositAmount float64
 		fmt.Scan(&depositAmount)
+
+		if depositAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than 0.")
+			return
+		}
+
 		accountBalance += depositAmount
 		fmt.Println("Balance updated, New amount: ", accountBalance)
 	} else if choice == 3 {
 		fmt.Print("Your withdraw: ")
 		var withdrawAmount float64
 		fmt.Scan(&withdrawAmount)
+		if withdrawAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than 0.")
+			return
+		}
+		if withdrawAmount > accountBalance {
+			fmt.Println("Invalid amount. You can't withdraw more than you have.")
+			return
+		}
 		accountBalance -= withdrawAmount
 		fmt.Println("Balance updated, New amount: ", accountBalance)
 	} else {
